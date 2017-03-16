60d0287d69b97b8dcac554a5dd019ae34e88c416
Jordan Walsh <jordan.walsh@.xero.com>
2017-03-16 11:58:30 +1100

    added currencies support

--- test/accountingtests.js
+++ test/accountingtests.js
@@ -498,7 +498,28 @@ describe('regression tests', function() {
                     done(wrapError(err));
                 })
         })
-    })
+    });
+
+    describe('currencies', function() {
+
+        it('get', function(done) {
+            currentApp.core.currencies.getCurrencies()
+                .then(function(currencies) {
+                    expect(currencies).to.have.length.greaterThan(0);
+                    currencies.forEach(function(currencies) {
+                        expect(currencies.Code).to.not.equal(undefined);
+                        expect(currencies.Code).to.not.equal("");
+                        expect(currencies.Description).to.not.equal(undefined);
+                        expect(currencies.Description).to.not.equal("");
+                    });
+                    done();
+                })
+                .catch(function(err) {
+                    console.log(err);
+                    done(wrapError(err));
+                })
+        });
+    });
 
     describe('branding themes', function() {
 
