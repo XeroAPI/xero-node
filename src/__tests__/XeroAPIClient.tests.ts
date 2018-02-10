e81805f1930483665ff6d427d3b1d5636cd42096
Phil Alsford <phil.alsford@xero.com>
2018-02-11 11:22:12 +1300

    Adds some contact group unit test

--- src/__tests__/XeroAPIClient.tests.ts
+++ src/__tests__/XeroAPIClient.tests.ts
@@ -1,14 +1,14 @@
-import { AccountingResponse, Invoice } from '../interfaces/AccountingResponse';
+import { AccountingResponse, Invoice, ContactGroups } from '../interfaces/AccountingResponse';
 import { XeroAPIClient } from '../XeroAPIClient';
 import * as path from 'path';
 import * as fs from 'fs';
 import { InMemoryOAuthClient } from './InMenoryOAuthClient';
 import { multipleInvoices } from './invoice.response.examples';
 import { isUUID } from './test-helpers';
+import { allContactGroups } from './contactgroups.response.examples';
 
 describe('/invoices', () => {
 	describe('and GETing', () => {
-
 		describe('multiple invoices', () => {
 			let result: AccountingResponse<Invoice>;
 
@@ -48,5 +48,39 @@ describe('/invoices', () => {
 		});
 
 	});
+});
 
+describe('/contactgroups', () => {
+	describe('and GETing', () => {
+		describe('all contact groups', () => {
+			let result: AccountingResponse<ContactGroups>;
+
+			beforeAll(async () => {
+
+				const privateKeyFile = path.resolve(__dirname + '/test-privatekey.pem');
+				const privateKey = fs.readFileSync(privateKeyFile, 'utf8');
+
+				const inMemoryOAuthClient = new InMemoryOAuthClient();
+				inMemoryOAuthClient.returnsWithNextGet(allContactGroups);
+
+				const xeroClient = new XeroAPIClient({
+					appType: 'private',
+					consumerKey: 'RDGDV41TRLQZDFSDX96TKQ2KRJIW4C',
+					consumerSecret: 'DJ3CMGDB0DIIA9DNEEJMRLZG0BWE7Y',
+					privateKey: privateKey
+				}, inMemoryOAuthClient);
+
+				result = await xeroClient.contactgroups.get();
+			});
+
+			it('the response is defined', () => {
+				expect(result).not.toBeNull();
+			});
+
+			it('matches the expected response', async () => {
+				expect(result).toMatchObject(allContactGroups);
+			});
+		});
+
+	});
 });
---
+++ src/__tests__/contactgroups.response.examples.ts
@@ -0,0 +1,33 @@
+const allContactGroups = {
+	Id: '021c4c92-2c05-4516-b7af-50286f8c147a',
+	Status: 'OK',
+	ProviderName: 'Xero API Previewer',
+	DateTimeUTC: '\/Date(1518300837185)\/',
+	ContactGroups: [
+		{
+			ContactGroupID: '3567ace4-1dc9-40b3-b364-9b55d5841b22',
+			Name: 'Contractors',
+			Status: 'ACTIVE',
+			Contacts: [],
+			HasValidationErrors: false
+		},
+		{
+			ContactGroupID: 'fb64fc23-d0f4-4236-a031-709d391df9e4',
+			Name: 'New Contacts 0.5082412871686646',
+			Status: 'ACTIVE',
+			Contacts: [],
+			HasValidationErrors: false
+		},
+		{
+			ContactGroupID: '26fcca8d-a03b-4968-a80a-a463d5bf30ee',
+			Name: 'Support Clients (monthly)',
+			Status: 'ACTIVE',
+			Contacts: [],
+			HasValidationErrors: false
+		}
+	]
+};
+
+export {
+	allContactGroups
+};
