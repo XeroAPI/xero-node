eae53ddc171dcf13c65ed5c7ba14f7d207212ffa
Phil Alsford <phil.alsford@xero.com>
2018-02-18 21:25:18 +1300

    Moves tests around ready for until testing of errors

--- src/__tests__/unit/XeroAPIClient-invoices.tests.ts
+++ src/__tests__/unit/XeroAPIClient-invoices.tests.ts
@@ -8,6 +8,9 @@ import { OAuthClient } from '../../OAuthClient';
 import { InMemoryOAuth } from './InMenoryOAuth';
 import { createSingleInvoiceRequest } from './request-examples/invoice.request.examples';
 
+const privateKeyFile = path.resolve(__dirname + '/test-privatekey.pem');
+const privateKey = fs.readFileSync(privateKeyFile, 'utf8');
+
 describe('/invoices', () => {
 	describe('and getting', () => {
 		describe('multiple invoices', () => {
@@ -15,10 +18,6 @@ describe('/invoices', () => {
 			const inMemoryOAuth = new InMemoryOAuth();
 
 			beforeAll(async () => {
-
-				const privateKeyFile = path.resolve(__dirname + '/test-privatekey.pem');
-				const privateKey = fs.readFileSync(privateKeyFile, 'utf8');
-
 				inMemoryOAuth.callbackResultsForNextCall(null, JSON.stringify(multipleInvoices), { statusCode: 200 });
 
 				const xeroClient = new XeroAPIClient({
@@ -55,45 +54,47 @@ describe('/invoices', () => {
 	});
 
 	describe('and creating', () => {
-		// TODO: This test actually mocks out the oauth lib and therefore runs all our code. We should migrate all tests to this?
-		let result: InvoicesResponse;
-		const inMemoryOAuth = new InMemoryOAuth();
+		describe('a single invoice', () => {
+			describe('successfully', () => {
+				let result: InvoicesResponse;
+				const inMemoryOAuth = new InMemoryOAuth();
 
-		beforeAll(async () => {
+				beforeAll(async () => {
+					inMemoryOAuth.callbackResultsForNextCall(null, JSON.stringify(singleInvoice), 201);
 
-			const privateKeyFile = path.resolve(__dirname + '/test-privatekey.pem');
-			const privateKey = fs.readFileSync(privateKeyFile, 'utf8');
+					const xeroClient = new XeroAPIClient({
+						appType: 'private',
+						consumerKey: 'RDGDV41TRLQZDFSDX96TKQ2KRJIW4C',
+						consumerSecret: 'DJ3CMGDB0DIIA9DNEEJMRLZG0BWE7Y',
+						privateKey: privateKey
+					}, null, inMemoryOAuth);
 
-			inMemoryOAuth.callbackResultsForNextCall(null, JSON.stringify(singleInvoice), 201);
+					result = await xeroClient.invoices.create(createSingleInvoiceRequest);
+				});
 
-			const xeroClient = new XeroAPIClient({
-				appType: 'private',
-				consumerKey: 'RDGDV41TRLQZDFSDX96TKQ2KRJIW4C',
-				consumerSecret: 'DJ3CMGDB0DIIA9DNEEJMRLZG0BWE7Y',
-				privateKey: privateKey
-			}, null, inMemoryOAuth);
+				it('the response is defined', () => {
+					expect(result).not.toBeNull();
+				});
 
-			result = await xeroClient.invoices.create(createSingleInvoiceRequest);
-		});
+				it('response.Id is a Guid and is actually the Id of the request', () => {
+					expect(isUUID(result.Id)).toBeTruthy();
+				});
 
-		it('the response is defined', () => {
-			expect(result).not.toBeNull();
-		});
+				it('there is more than one invoice', () => {
+					expect(result.Invoices.length).toBe(1);
+				});
 
-		it('response.Id is a Guid and is actually the Id of the request', () => {
-			expect(isUUID(result.Id)).toBeTruthy();
-		});
+				it('matches the expected response', () => {
+					expect(result).toMatchObject(singleInvoice);
+				});
 
-		it('there is more than one invoice', () => {
-			expect(result.Invoices.length).toBe(1);
-		});
+				it('called the correct URL', () => {
+					inMemoryOAuth.lastCalledThisURL('https://api.xero.com/api.xro/2.0/invoices?summarizeErrors=false');
+				});
+			});
 
-		it('matches the expected response', () => {
-			expect(result).toMatchObject(singleInvoice);
-		});
-
-		it('called the correct URL', () => {
-			inMemoryOAuth.lastCalledThisURL('https://api.xero.com/api.xro/2.0/invoices?summarizeErrors=false');
+			// TODO: Here.
+			describe('unsuccessfully', () => {});
 		});
 	});
 });
