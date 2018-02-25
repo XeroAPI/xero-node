d3c6162232bddb833651dfc9a8cda545d06ca4e2
Bryan Lloyd Tee <bryan.tee@xero.com>
2018-02-26 09:00:58 +1300

    added employees tests

--- src/__tests__/unit/XeroAPIClient-employees.tests.ts
+++ src/__tests__/unit/XeroAPIClient-employees.tests.ts
@@ -1,10 +1,11 @@
+import { Employee } from './../../interfaces/AccountingResponse.d';
 import { AccountingResponse, Employee, EmployeesResponse } from '../../interfaces/AccountingResponse';
 import { XeroAPIClient } from '../../XeroAPIClient';
 import * as path from 'path';
 import * as fs from 'fs';
 import { isUUID } from '../test-helpers';
 import { InMemoryOAuth } from './InMenoryOAuth';
-import { allEmployeesResponse } from './response-examples/employees.response.examples';
+import { allEmployeesResponse, createResponse } from './response-examples/employees.response.examples';
 
 const privateKeyFile = path.resolve(__dirname + '/test-privatekey.pem');
 const privateKey = fs.readFileSync(privateKeyFile, 'utf8');
@@ -43,40 +44,41 @@ describe('/employees', () => {
 
 	});
 
-	// describe('and creating', () => {
-	// 	describe('an employee', () => {
-	// 		let result: EmployeesResponse;
-	// 		const inMemoryOAuth = new InMemoryOAuth();
+	describe('and creating', () => {
+		describe('an employee', () => {
+			let result: EmployeesResponse;
+			const inMemoryOAuth = new InMemoryOAuth();
 
-	// 		beforeAll(async () => {
-	// 			inMemoryOAuth.callbackResultsForNextCall(null, JSON.stringify(createResponse), {stausCode: 200});
+			beforeAll(async () => {
+				inMemoryOAuth.callbackResultsForNextCall(null, JSON.stringify(createResponse), {stausCode: 200});
 
-	// 			const xeroClient = new XeroAPIClient({
-	// 				appType: 'private',
-	// 				consumerKey: 'RDGDV41TRLQZDFSDX96TKQ2KRJIW4C',
-	// 				consumerSecret: 'DJ3CMGDB0DIIA9DNEEJMRLZG0BWE7Y',
-	// 				privateKey: privateKey
-	// 			}, null, inMemoryOAuth);
+				const xeroClient = new XeroAPIClient({
+					appType: 'private',
+					consumerKey: 'RDGDV41TRLQZDFSDX96TKQ2KRJIW4C',
+					consumerSecret: 'DJ3CMGDB0DIIA9DNEEJMRLZG0BWE7Y',
+					privateKey: privateKey
+				}, null, inMemoryOAuth);
 
-	// 			const currency: Currency = {
-	// 				Code: 'HKD'
-	// 			};
+				const employee: Employee = {
+					FirstName: 'Bryan',
+					LastName: 'Tee'
+				};
 
-	// 			result = await xeroClient.currencies.create(currency);
-	// 		});
+				result = await xeroClient.employees.create(employee);
+			});
 
-	// 		it('the response is defined', () => {
-	// 			expect(result).not.toBeNull();
-	// 		});
+			it('the response is defined', () => {
+				expect(result).not.toBeNull();
+			});
 
-	// 		it('matches the expected response', () => {
-	// 			expect(result).toMatchObject(createResponse);
-	// 		});
+			it('matches the expected response', () => {
+				expect(result).toMatchObject(createResponse);
+			});
 
-	// 		it('called the correct URL', () => {
-	// 			inMemoryOAuth.lastCalledThisURL('https://api.xero.com/api.xro/2.0/currencies');
-	// 		});
-	// 	});
+			it('called the correct URL', () => {
+				inMemoryOAuth.lastCalledThisURL('https://api.xero.com/api.xro/2.0/employees');
+			});
+		});
 
-	// });
+	});
 });
