6621697668342401767770ff4c0cd941200b1841
Phil Alsford <phil.alsford@xero.com>
2018-03-02 16:12:04 +1300

    script

--- .circleci/create_private_key.js
+++ .circleci/create_private_key.js
@@ -1,4 +1,4 @@
 let fs = require('fs');
 const testFolder = './src/__integration_tests__/';
 // I have base64 encoded the ley on CCI as multi line env vars are not well supported in CCI.
-fs.writeFileSync(testFolder + 'privatekey.pem', Buffer.from(process.env.PRIVATE_KEY, 'base64'));
+fs.writeFileSync(testFolder + 'helpers/privatekey.pem', Buffer.from(process.env.PRIVATE_KEY, 'base64'));
