a6b1e35b59031d00e14638eb066a6edd4e2a1521
Phil Alsford <phil.alsford@xero.com>
2018-03-12 08:50:05 +1300

    Adds full partner example with UI

--- src/__integration_tests__/partner-example.ts
+++ src/__integration_tests__/partner-example.ts
@@ -1,18 +1,55 @@
 import { AccountingAPIClient } from '../AccountingAPIClient';
-import * as opn from 'opn';
-import { readLine } from './helpers/integration.helpers';
+import * as puppeteer from 'puppeteer';
+
+// TODO: Turn this into a Jest test
+// TODO: Have it run in CI
 
 (async function main() {
 	const config = require('../partner-config.json');
+	// Needs your Xero password so that it can auth an Org
+	const password_config = require('./password-config.json');
+
+	// password-config must be in this format
+	// {
+	// 	"userName": "email here",
+	// 		"password": "passwordHere"
+	// }
+
 	const accounting1 = new AccountingAPIClient(config);
 
 	try {
 		await accounting1.oauth1.getUnauthorisedRequestToken();
 		const url = await accounting1.oauth1.buildAuthoriseUrl();
 
-		opn(url);
-		console.log('Copy and past the pin from the auth page:');
-		const pin = await readLine('PIN: ');
+		const browser = await puppeteer.launch({ headless: false });
+		const page = await browser.newPage();
+		await page.goto(url);
+
+		const USERNAME_SELECTOR = '#email';
+		const PASSWORD_SELECTOR = '#password';
+		const LOGIN_BUTTON_SELECTOR = '#submitButton';
+		const AUTH_BUTTON_SELECTOR = '#submit-button';
+
+		await page.click(USERNAME_SELECTOR);
+		await page.keyboard.type(password_config.userName);
+
+		await page.click(PASSWORD_SELECTOR);
+		await page.keyboard.type(password_config.password);
+
+		await page.click(LOGIN_BUTTON_SELECTOR);
+		await page.waitForNavigation();
+		await page.waitForNavigation();
+
+		await page.click(AUTH_BUTTON_SELECTOR);
+		await page.waitForNavigation();
+
+		await delay(2500);
+
+		const pin = await page.evaluate(() => {
+			const PIN_SELECTOR = '#pin-input';
+			const query = (document.querySelector(PIN_SELECTOR) as any).value;
+			return query;
+		});
 
 		await accounting1.oauth1.swapRequestTokenforAccessToken(pin);
 		const inv1 = await accounting1.invoices.get();
@@ -37,7 +74,14 @@ import { readLine } from './helpers/integration.helpers';
 		// Now we can make the same request
 		const inv4 = await accounting1.invoices.get();
 		console.log('Number of invoices (4): ', inv4.Invoices.length);
+		page.close();
 	} catch (error) {
 		console.log('ERROR: ', error);
 	}
 })();
+
+function delay(timeout: number) {
+	return new Promise((resolve) => {
+		setTimeout(resolve, timeout);
+	});
+}
