cd725d0312f6ecdbdb6225faa2aa29cfd92e4613
dev-bryan.tee <bryan.tee@xero.com>
2018-03-13 16:49:13 +1300

    set timeouts to individual tests

--- src/__integration_tests__/partner-example.tests.ts
+++ src/__integration_tests__/partner-example.tests.ts
@@ -42,7 +42,7 @@ describe('Partner Example Tests', () => {
 			headless: true,
 		});
 		page = await browser.newPage();
-		page.setDefaultNavigationTimeout(60000);
+		page.setDefaultNavigationTimeout(120000);
 		await page.goto(authUrl);
 	
 		await page.click(USERNAME_SELECTOR);
@@ -73,23 +73,23 @@ describe('Partner Example Tests', () => {
 
 	it('it returns the authorised url', async () => {
 		expect(authUrl).toContain('xero.com');
-	});
+	}, 120000);
 
 	it('it returns a PIN when the user allows access to the app', async () => {
 		expect(pin).not.toBeNull();
-	});
+	}), 120000;
 
 	it('it can make a successful API call', async () => {
 		await accounting1.oauth1.swapRequestTokenforAccessToken(pin);
 		const inv1 = await accounting1.invoices.get();
 		expect(inv1.Status).toEqual('OK');
-	});
+	}, 120000);
 
 	it('it can still make a successfull API call after refreshing the access token', async () => {
 		await accounting1.oauth1.refreshAccessToken();
 		const inv2 = await accounting1.invoices.get();
 		expect(inv2.Status).toEqual('OK');
-	});
+	}, 120000);
 
 	describe('OAuth State', () => {
 		let state: IOAuth1State;
@@ -99,18 +99,18 @@ describe('Partner Example Tests', () => {
 			expect(state.accessToken).not.toBeNull();
 			expect(state.oauth_session_handle).not.toBeNull();
 			expect(state.requestToken).not.toBeNull();
-		  })
+		  }, 120000)
 		  
 		  it('it allows you to restore a new instance of the client next time your user logs in', async () => {
 			accounting2 = new AccountingAPIClient(config);
 			accounting2.oauth1.setState(state);
 			expect(accounting2.oauth1.getState()).toMatchObject(state);
-		  });
+		  }, 120000);
 
 		  it('it lets you make API calls using the restored state', async () => {
 			const inv3 = await accounting2.invoices.get();
 			expect(inv3.Status).toEqual('OK');
-		  });
+		  }, 120000);
 	})
 
 });
