2985f6427f9140f932197308f80710cde94605bb
dev-bryan.tee <bryan.tee@xero.com>
2018-03-20 10:21:46 +1300

    added users integration test

---
+++ src/__integration_tests__/users.integration.tests.ts
@@ -0,0 +1,34 @@
+import { AccountingAPIClient } from '../AccountingAPIClient';
+import { getPrivateConfig, setJestTimeout } from './helpers/integration.helpers';
+import { UsersResponse } from '../AccountingAPI-types';
+
+describe('users', () => {
+
+	let xero: AccountingAPIClient;
+	let response: UsersResponse;
+
+	beforeAll(() => {
+		setJestTimeout();
+		const config = getPrivateConfig();
+		xero = new AccountingAPIClient(config);
+	});
+
+	it('get all', async () => {
+		response = await xero.users.get();
+
+		expect(response).toBeDefined();
+		expect(response.Id).toBeTruthy();
+		expect(response.Users.length).toBeGreaterThan(0);
+		expect(response.Users[0].UserID).toBeTruthy();
+	});
+
+	it('get single', async () => {
+		const singleResponse = await xero.users.get({ UserID: response.Users[0].UserID});
+
+		expect(singleResponse).toBeDefined();
+		expect(singleResponse.Id).toBeTruthy();
+		expect(singleResponse.Users.length).toBeGreaterThan(0);
+		expect(singleResponse.Users[0].UserID).toBeTruthy();
+	});
+
+});
