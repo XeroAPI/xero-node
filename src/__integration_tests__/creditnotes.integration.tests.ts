0394f7dce065a7a79a702d394f3ccede8474b57c
Phil Alsford <phil.alsford@xero.com>
2018-03-22 12:37:03 +1300

    Example for creditnotes

---
+++ src/__integration_tests__/creditnotes.integration.tests.ts
@@ -0,0 +1,24 @@
+import { AccountingAPIClient } from '../AccountingAPIClient';
+import { getPrivateConfig, setJestTimeout } from './helpers/integration.helpers';
+import { CreditNotesResponse } from '../../lib/AccountingAPI-types';
+
+describe('/creditnotes', () => {
+	let xero: AccountingAPIClient;
+	let response: CreditNotesResponse;
+
+	beforeAll(async () => {
+		setJestTimeout();
+		const config = getPrivateConfig();
+		xero = new AccountingAPIClient(config);
+	});
+
+	it('can get all', async () => {
+		response = await xero.creditNotes.get();
+		expect(response.CreditNotes[0].CreditNoteID).toBeDefined();
+	});
+
+	it('can get single', async () => {
+		await xero.creditNotes.get({ CreditNoteID: response.CreditNotes[0].CreditNoteID });
+		expect(response.CreditNotes[0].CreditNoteID).toBeDefined();
+	});
+});
