6c9e0c98290880d367845159a17c74a415ffcc45
Russell Briggs <russell.briggs@xero.com>
2018-04-06 16:39:25 +1200

    Add nicer typedoc theme

--- typedoc.js
+++ typedoc.js
@@ -1,10 +1,10 @@
 module.exports = {
     mode: 'file',
     includes: 'src/',
-    exclude: '**/{__tests__,lib}/**/*',
+    exclude: '**/__tests__/**/*',
     out: 'docs/v3/',
+	theme: 'docs_theme/',
     excludeExternals: true,
     excludeNotExported: true,
-	excludePrivate: true,
-	//includeDeclarations: true
+	excludePrivate: true
 };
