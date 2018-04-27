7ddf25ac6f7a5021d9e8f321c4b66cc804272d9b
Phil Alsford <phil.alsford@xero.com>
2018-04-27 20:17:22 +1200

    Finshes example

--- src/__integration_tests__/partner-example-callbackUrl.tests.ts
+++ src/__integration_tests__/partner-example-callbackUrl.tests.ts
@@ -62,47 +62,45 @@ describe('Partner Example Tests with callbackUrl', () => {
 		browser.close();
 	});
 
-	describe('Partner Example Tests with callbackUrl', async () => {
-		it('it returns the authorised url', async () => {
-			expect(authUrl).toContain('xero.com');
+	it('it returns the authorised url', async () => {
+		expect(authUrl).toContain('xero.com');
+	});
+
+	it('it returns a PIN when the user allows access to the app', async () => {
+		expect(oauth_verifier).not.toBeNull();
+	});
+
+	it('it can make a successful API call', async () => {
+		authState = await accounting1.oauth1Client.swapRequestTokenforAccessToken(requestToken, oauth_verifier);
+		const inv1 = await accounting1.oauth1Client.get('/api.xro/2.0/Organisation') as any;
+		expect(inv1.Status).toEqual('OK');
+	});
+
+	it('it can still make a successfull API call after refreshing the access token', async () => {
+		authState = await accounting1.oauth1Client.refreshAccessToken();
+		const inv2 = await accounting1.oauth1Client.get('/api.xro/2.0/Organisation') as any;
+		expect(inv2.Status).toEqual('OK');
+	});
+
+	describe('OAuth State', () => {
+		let accounting2_callback: BaseAPIClient;
+		it('it allows you to keep copy of the state in your own dadtastore', async () => {
+			// Saves your state to your datastore
+			expect(authState).not.toBeNull();
+			expect(authState.oauth_session_handle).not.toBeNull();
+			expect(requestToken).not.toBeNull();
+
+			// This is how you can check when you have to refresh your Access Token
+			expect(typeof authState.oauth_expires_at.getDate).toBe('function');
 		});
 
-		it('it returns a PIN when the user allows access to the app', async () => {
-			expect(oauth_verifier).not.toBeNull();
+		it('it allows you to restore a new instance of the client next time your user logs in', async () => {
+			accounting2_callback = new BaseAPIClient(config, authState);
 		});
 
-		it('it can make a successful API call', async () => {
-			authState = await accounting1.oauth1Client.swapRequestTokenforAccessToken(requestToken, oauth_verifier);
-			const inv1 = await accounting1.oauth1Client.get('/api.xro/2.0/Organisation') as any;
-			expect(inv1.Status).toEqual('OK');
-		});
-
-		it('it can still make a successfull API call after refreshing the access token', async () => {
-			await accounting1.oauth1Client.refreshAccessToken();
-			const inv2 = await accounting1.oauth1Client.get('/api.xro/2.0/Organisation') as any;
-			expect(inv2.Status).toEqual('OK');
-		});
-
-		describe('OAuth State', () => {
-			let accounting2_callback: BaseAPIClient;
-			it('it allows you to keep copy of the state in your own dadtastore', async () => {
-				// Saves your state to your datastore
-				expect(authState).not.toBeNull();
-				expect(authState.oauth_session_handle).not.toBeNull();
-				expect(requestToken).not.toBeNull();
-
-				// This is how you can check when you have to refresh your Access Token
-				expect(typeof authState.oauth_expires_at.getDate).toBe('function');
-			});
-
-			it('it allows you to restore a new instance of the client next time your user logs in', async () => {
-				accounting2_callback = new BaseAPIClient(config, authState);
-			});
-
-			it('it lets you make API calls using the restored state', async () => {
-				const inv3 = await accounting1.oauth1Client.get('/api.xro/2.0/Organisation') as any;
-				expect(inv3.Status).toEqual('OK');
-			});
+		it('it lets you make API calls using the restored state', async () => {
+			const inv3 = await accounting2_callback.oauth1Client.get('/api.xro/2.0/Organisation') as any;
+			expect(inv3.Status).toEqual('OK');
 		});
 	});
 });
