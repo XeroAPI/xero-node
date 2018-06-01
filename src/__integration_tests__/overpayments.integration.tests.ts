6e30a01cbbd1eb963bdb12a5254e13e6f3c04a22
Bruce Martin <bruceandnatasha@gmail.com>
2018-06-01 07:08:09 +0200

    Added OverpaymentID for history test

--- src/__integration_tests__/overpayments.integration.tests.ts
+++ src/__integration_tests__/overpayments.integration.tests.ts
@@ -3,6 +3,7 @@ import { getPrivateConfig, setJestTimeout } from './helpers/integration.helpers'
 
 describe('/overpayments', () => {
 	let xero: AccountingAPIClient;
+	let overPaymentID: any;
 	
 	beforeAll(async () => {
 		setJestTimeout();
@@ -15,10 +16,11 @@ describe('/overpayments', () => {
 		expect(response).not.toBeNull();
 		expect(response.Overpayments).toBeInstanceOf(Array);
 		expect(response.Overpayments[0]).toHaveProperty('Contact');
+		overPaymentID = response.Overpayments[0].OverpaymentID;
 	});
 
 	it('get history', async () => {
-		const response = await xero.overpayments.history.get();
+		const response = await xero.overpayments.history.get(overPaymentID);
 		
 		expect(response.HistoryRecords[0]).toBeDefined();
 	});
