ef1c8938c140618bc3292a6635a655bd27d9e775
James Wilson <james.wilson@potentiate.com>
2018-06-29 18:32:10 +1000

    Feature #215. Not tests done

--- src/internals/OAuth1HttpClient.ts
+++ src/internals/OAuth1HttpClient.ts
@@ -444,6 +444,32 @@ export class OAuth1HttpClient implements IOAuth1HttpClient {
 		});
 	}
 
+	public patch = async <T>(endpoint: string, body: object, customHeaders?: { [key: string]: string }): Promise<T> => {
+		this.resetToDefaultHeaders();
+		this.oauthLib._headers = { ...this._defaultHeaders, ...customHeaders };
+		this.assertAccessTokenIsSet();
+		return new Promise<T>((resolve, reject) => {
+			this.oauthLib.patch(
+				this.config.apiBaseUrl + this.config.apiBasePath + endpoint, // url
+				this._state.oauth_token,
+				this._state.oauth_token_secret,
+				JSON.stringify(body), 		// Had to do this not sure if there is another way
+				'application/json',
+				(err: any, data: string, httpResponse: any) => {
+					// data is the body of the response
+
+					if (err) {
+						reject(err.statusCode ? new XeroError(err.statusCode, err.data, httpResponse ? httpResponse.headers : null) : err);
+					} else {
+						const toReturn = JSON.parse(data) as T;
+						return resolve(toReturn);
+					}
+				}
+			);
+
+		});
+	}
+
 	private setState(newState: Partial<AccessToken>) {
 		this._state = { ...this._state, ...newState };
 	}
