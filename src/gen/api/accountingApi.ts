f0efc0e11b120ee6b63610cee5722739c9622060
SidneyAllen <sid.maestre@gmail.com>
2020-01-21 12:00:02 -0800

    Add vendorExtension for empty response
    check for x-isEmpty and add the json:true option  - to allow the RequestEmpty to pass a JSON object in the body.

--- src/gen/api/accountingApi.ts
+++ src/gen/api/accountingApi.ts
@@ -4674,7 +4674,7 @@ export class AccountingApi {
             qs: localVarQueryParameters,
             headers: localVarHeaderParams,
             uri: localVarPath,
-            useQuerystring: this._useQuerystring,
+            useQuerystring: this._useQuerystring,json: true,
             body: ObjectSerializer.serialize(requestEmpty, "RequestEmpty")
         };
 
