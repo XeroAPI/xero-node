3d89ce6ecb69fece177d34b15d8d30680fd682b2
Rett Behrens <rett@rettbehrens.codes>
2021-10-29 13:00:01 -0600

    generates from OAS 2.17.2 to capture updates to contacts endpoint

--- src/gen/model/accounting/models.ts
+++ src/gen/model/accounting/models.ts
@@ -290,6 +290,8 @@ let enumsMap: {[index: string]: any} = {
         "BrandingTheme.TypeEnum": BrandingTheme.TypeEnum,
         "Budget.TypeEnum": Budget.TypeEnum,
         "Contact.ContactStatusEnum": Contact.ContactStatusEnum,
+        "Contact.SalesDefaultLineAmountTypeEnum": Contact.SalesDefaultLineAmountTypeEnum,
+        "Contact.PurchasesDefaultLineAmountTypeEnum": Contact.PurchasesDefaultLineAmountTypeEnum,
         "ContactGroup.StatusEnum": ContactGroup.StatusEnum,
         "CountryCode": CountryCode,
         "CreditNote.TypeEnum": CreditNote.TypeEnum,
