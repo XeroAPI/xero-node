c9f685d8dd256046036e5029b9e1861d3747c911
manishT72x <manish.tanwar@xero.com>
2024-06-21 17:55:11 +0530

    Build from OAS 4.0.0

--- src/gen/model/files/association.ts
+++ src/gen/model/files/association.ts
@@ -3,6 +3,18 @@ import { ObjectType } from '././objectType';
 
 export class Association {
     /**
+    * Boolean flag to determines whether the file is sent with the document it is attached to on client facing communications. Note- The SendWithObject element is only returned when using /Associations/{ObjectId} endpoint.
+    */
+    'sendWithObject'?: boolean;
+    /**
+    * The name of the associated file. Note- The Name element is only returned when using /Associations/{ObjectId} endpoint.
+    */
+    'name'?: string;
+    /**
+    * The size of the associated file in bytes. Note- The Size element is only returned when using /Associations/{ObjectId} endpoint.
+    */
+    'size'?: number;
+    /**
     * The unique identifier of the file
     */
     'fileId'?: string;
@@ -16,6 +28,21 @@ export class Association {
     static discriminator: string | undefined = undefined;
 
     static attributeTypeMap: Array<{name: string, baseName: string, type: string}> = [
+        {
+            "name": "sendWithObject",
+            "baseName": "SendWithObject",
+            "type": "boolean"
+        },
+        {
+            "name": "name",
+            "baseName": "Name",
+            "type": "string"
+        },
+        {
+            "name": "size",
+            "baseName": "Size",
+            "type": "number"
+        },
         {
             "name": "fileId",
             "baseName": "FileId",
