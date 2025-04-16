4f5ab1912b2e23c57875310d07e17cebe82c3379
sangeet-joy_xero <sangeet.joy@xero.com>
2025-04-16 10:04:40 +0530

    Package version updated

--- src/gen/api/assetApi.ts
+++ src/gen/api/assetApi.ts
@@ -37,7 +37,7 @@ export enum AssetApiApiKeys {
 
 export class AssetApi {
     protected _basePath = defaultBasePath;
-    protected defaultHeaders : any = {'user-agent': 'xero-node-11.0.0'};
+    protected defaultHeaders : any = {'user-agent': 'xero-node-11.1.0'};
     protected _useQuerystring : boolean = false;
     protected binaryHeaders : any = {};
 
