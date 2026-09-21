64bf7e0de68005e7ac95663b8e0b41028a6984ba
ryanNexus <ryanwork401@gmail.com>
2026-09-21 16:08:36 +1200

    [XAPI-2608] fix version: 14.1.1 -> 14.1.0

--- src/gen/api/payrollAUV2Api.ts
+++ src/gen/api/payrollAUV2Api.ts
@@ -38,7 +38,7 @@ export enum PayrollAuV2ApiApiKeys {
 
 export class PayrollAuV2Api {
     protected _basePath = defaultBasePath;
-    protected defaultHeaders : any = {'user-agent': 'xero-node-14.1.1'};
+    protected defaultHeaders : any = {'user-agent': 'xero-node-14.1.0'};
     protected _useQuerystring : boolean = false;
     protected binaryHeaders : any = {};
 
