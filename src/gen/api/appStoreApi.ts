8b69642792db932d7cc288726813e422bcd793b5
ryanNexus <ryanwork401@gmail.com>
2026-09-21 14:56:47 +1200

    [XAPI-2608] version bump for docs

--- src/gen/api/appStoreApi.ts
+++ src/gen/api/appStoreApi.ts
@@ -38,7 +38,7 @@ export enum AppStoreApiApiKeys {
 
 export class AppStoreApi {
     protected _basePath = defaultBasePath;
-    protected defaultHeaders : any = {'user-agent': 'xero-node-13.1.0'};
+    protected defaultHeaders : any = {'user-agent': 'xero-node-13.4.1'};
     protected _useQuerystring : boolean = false;
     protected binaryHeaders : any = {};
 
