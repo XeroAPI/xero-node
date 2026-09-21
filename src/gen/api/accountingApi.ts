6ab041dd3bb11e4f3a69c8c1c5dc360a253ac947
ryanNexus <ryanwork401@gmail.com>
2026-09-21 16:01:47 +1200

    backport: redact request headers from error objects in 16.1.0 (#846)

--- src/gen/api/accountingApi.ts
+++ src/gen/api/accountingApi.ts
@@ -84,7 +84,7 @@ export enum AccountingApiApiKeys {
 
 export class AccountingApi {
     protected _basePath = defaultBasePath;
-    protected defaultHeaders : any = {'user-agent': 'xero-node-16.0.0'};
+    protected defaultHeaders : any = {'user-agent': 'xero-node-16.1.0'};
     protected _useQuerystring : boolean = false;
     protected binaryHeaders : any = {};
 
--- src/gen/api/financeApi.ts
+++ src/gen/api/financeApi.ts
@@ -40,7 +40,7 @@ export enum FinanceApiApiKeys {
 
 export class FinanceApi {
     protected _basePath = defaultBasePath;
-    protected defaultHeaders : any = {'user-agent': 'xero-node-16.0.0'};
+    protected defaultHeaders : any = {'user-agent': 'xero-node-16.1.0'};
     protected _useQuerystring : boolean = false;
     protected binaryHeaders : any = {};
 
