19f1c2a0e031bbfc2af617de1fc97fddc351eddc
ryanNexus <ryanwork401@gmail.com>
2026-09-21 14:13:19 +1200

    [XAPI-2608] Adding patch for 18.0.0

--- src/XeroClient.ts
+++ src/XeroClient.ts
@@ -1,5 +1,6 @@
 import { Client, Issuer, TokenSet, TokenSetParameters, custom } from 'openid-client';
 import * as xero from './gen/api';
+import { redactError } from './model/ApiError';
 const axios = require('axios');
 import http = require('http');
 
@@ -220,7 +221,7 @@ export class XeroClient {
         }
       }
       catch (error) {
-        reject(error)
+        reject(redactError(error))
       }
     });
   }
@@ -262,7 +263,7 @@ export class XeroClient {
         }
       }
       catch (error) {
-        reject(error)
+        reject(redactError(error))
       }
     });
   }
