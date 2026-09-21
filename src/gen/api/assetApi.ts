f95c0f365c6ade5093b9b04deb8bda6521992b8f
ryanNexus <ryanwork401@gmail.com>
2026-09-21 15:21:07 +1200

    [XAPI-2608] version bump for docs

--- src/gen/api/assetApi.ts
+++ src/gen/api/assetApi.ts
@@ -37,7 +37,7 @@ export enum AssetApiApiKeys {
 
 export class AssetApi {
     protected _basePath = defaultBasePath;
-    protected defaultHeaders : any = {'user-agent': 'xero-node-15.0.1'};
+    protected defaultHeaders : any = {'user-agent': 'xero-node-15.1.1'};
     protected _useQuerystring : boolean = false;
     protected binaryHeaders : any = {};
 
--- src/gen/api/financeApi.ts
+++ src/gen/api/financeApi.ts
@@ -40,7 +40,7 @@ export enum FinanceApiApiKeys {
 
 export class FinanceApi {
     protected _basePath = defaultBasePath;
-    protected defaultHeaders : any = {'user-agent': 'xero-node-15.0.1'};
+    protected defaultHeaders : any = {'user-agent': 'xero-node-15.1.1'};
     protected _useQuerystring : boolean = false;
     protected binaryHeaders : any = {};
 
